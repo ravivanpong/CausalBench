12c0bd9181b3b2b21459325e181c7ffce3965f22
HuiGong-dev <hellohuigong@gmail.com>
2022-12-04 18:09:43 +0100

    add pc from gcastle

---
+++ experiments/example.py
@@ -0,0 +1,87 @@
+'''
+Example for experiment.
+'''
+import os, sys
+import gc
+import time
+import concurrent.futures
+
+file_dir = os.path.dirname(__file__)
+causalbench_dir = os.path.join(file_dir, '..')
+path_result = os.path.join(file_dir, 'result')
+sys.path.append(causalbench_dir)
+
+from causalbench.utils.common import *
+from causalbench.algorithms.gcastle.PC_castle import PC_castle
+
+
+# these datasets are from cdt
+list_dataset = ["dream4-1", "dream4-2", "dream4-3", "dream4-4", "dream4-5","sachs"] 
+list_standardize = [False, True]
+list_algo = [PC_castle()]
+task_list = combine_multiple_lists([list_algo, list_dataset, list_standardize])
+
+# parameters for algorithem
+#could import as json file in the future
+dict_algo_param = {
+    'castle': {
+        'PC': {
+            'parameters': {
+                'variant': 'stable',
+            }
+        }
+    }
+}
+
+def run(alg, dataset_name, standardize, dict_algo_param):
+    # load data
+    data, true_graph, true_adj_matrix = load_data_from_cdt(dataset_name)
+    if standardize:
+        data = standardize_data(data)
+    #varsort = calc_varsortability(data, true_adj_matrix)
+
+    # estimate
+    #print(f"Start to estimate for: \n dataset_name = {dataset_name} \n standardize = {standardize} \n alg = {alg.name}\n")
+    starttime = time.perf_counter()
+    estimated_adj_matrix = alg.fit(data, dict_algo_param[alg.lib][alg.name])
+    finishtime = time.perf_counter()
+    runtime = round(finishtime - starttime, 2)
+
+    # evaluate
+    shd, shd_cpdag, auc, curve = evaluate_cdt_metrics(estimated_adj_matrix, true_graph)
+
+    # output
+    dict_result = {
+        "dataset_name": dataset_name
+        ,"N_variables": data.shape[1]
+        ,"N_obs": data.shape[0]
+        ,"standardized": standardize
+        ,"varsortability": 'notyet'
+        ,"model_name": alg.name
+        ,"SHD": float(shd)
+        ,"SHD_CPAG": float(shd_cpdag)
+        ,"AOC": 1 - auc
+        ,"runtime_second": runtime}
+    #print("Summary:\n", dict_result)
+
+    gen_output_file(path_result, "benchmark_result.csv", dict_result )
+    gc.collect()
+
+def main():
+    start = time.perf_counter()
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        for task in task_list:
+            executor.submit(run, task[0], task[1], task[2], dict_algo_param)
+    finish = time.perf_counter()
+    print(f"benchmarking finished in {round(finish - start, 2)} second(s)")   
+
+if __name__ == '__main__':
+    main()         
+
+
+
+
+
+
+
+
