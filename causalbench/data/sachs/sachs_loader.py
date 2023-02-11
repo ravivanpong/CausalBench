ccb0f21f07cffaa09ae2b9ee9b89c10c22cba665
HuiGong-dev <hellohuigong@gmail.com>
2023-02-11 20:14:20 +0100

    sachs from cdt

--- causalbench/data/sachs/sachs_loader.py
+++ causalbench/data/sachs/sachs_loader.py
@@ -1,39 +1,52 @@
-import zipfile
+import os
+from zipfile import ZipFile
+from io import BytesIO
 import pandas as pd
+import numpy as np
 from causalbench.metrics.varsortability import varsortability
 
-# unzip
-def unzip(from_path, to_dir):
-    with zipfile.ZipFile(
-        from_path,
-        "r",
-    ) as zip_ref:
-        zip_ref.extractall(to_dir)
 
+def load_sachs():
+    # read from zip file
+    sachs_target_bytes = None
+    sachs_data_bytes = None
+    dirname = os.path.dirname(os.path.realpath(__file__))
+    with ZipFile(f"{dirname}/sachs.zip") as zip_archive:
+        sachs_target_bytes = zip_archive.read("sachs/cyto_full_target.csv")
+        sachs_data_bytes = zip_archive.read("sachs/cyto_full_data.csv")
+    # convert bytes into dadaframe
+    true_graph_df = pd.read_csv(BytesIO(sachs_target_bytes))
+    data_df = pd.read_csv(BytesIO(sachs_data_bytes))
+    # build true graph matrix
+    nodes = list(data_df.columns)
+    edges = []
+    for _, row in true_graph_df.iterrows():
+        edges.append((row["Cause"], row["Effect"]))
 
-def sachs_loader(path_to_dataset):
-    true_causal_matrix_df = pd.read_fwf(
-        "/Users/huigong/ba-thesis/CausalBench/causalbench/data/sachs/ground_truth.txt",
-        header=None,
-    )
-    true_causal_matrix = true_causal_matrix_df.to_numpy()
-    X_df = pd.read_fwf(path_to_dataset, skiprows=1, header=None)
-    X = X_df.to_numpy()
+    node_index_map = {}
+    i = 0
+    for node in nodes:
+        node_index_map[node] = i
+        i += 1
+
+    n_nodes = len(nodes)
+
+    true_matrix = np.zeros((n_nodes, n_nodes))
+    for edge in edges:
+        true_matrix[node_index_map[edge[0]]][node_index_map[edge[1]]] = 1
+
+    data = data_df.to_numpy()
+
     result = {}
-    result["true_causal_matrix"] = true_causal_matrix
-    result["X"] = X
-    result["var_num"] = X.shape[1]
-    result["sample_num"] = X.shape[0]
-    result["varsortability"] = varsortability(X, true_causal_matrix)
-    print(result["varsortability"])
+    result["true_matrix"] = true_matrix
+    result["X"] = data
+    result["var_num"] = data.shape[1]
+    result["sample_num"] = data.shape[0]
+    result["varsortability"] = varsortability(data, true_matrix)
     return result
 
 
-# unzip(
-#     "/Users/huigong/ba-thesis/CausalBench/causalbench/data/sachs/real-sachs.zip",
-#     "/Users/huigong/ba-thesis/CausalBench/causalbench/data/sachs",
-# )
-
-sachs = sachs_loader(
-    "/Users/huigong/ba-thesis/CausalBench/causalbench/data/sachs/data/sachs.2005.discrete.txt"
-)
+# sachs = load_sachs()
+# print(sachs["var_num"])
+# print(sachs["varsortability"])
+# print(sachs["sample_num"])
