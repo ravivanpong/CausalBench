dfd52a5268b58862012da05541ea037b8466d1db
HuiGong-dev <hellohuigong@gmail.com>
2023-02-23 13:49:49 +0100

    support argparse and use json file to set up experiment

--- experiments/castle/experiment_castle_json_args.py
+++ experiments/castle/experiment_castle_json_args.py
@@ -1,171 +1,151 @@
 import time
 import logging
 import os
+import json
+import argparse
 from csv import DictWriter
 import concurrent.futures
 from castle.metrics import MetricsDAG
 from causalbench.utils.helper import combine_multiple_lists
 
-###################### Edit blow area to set up experiment. ##############################
-# step 1: set dataset and it's parameters
-dataset_list = {
-    "alarm": {"index": 3, "sample_num": 500, "version": 1},
-    "dream4": {"version": 1},
-    "jdk": {},
-    "postgres": {},
-    "sachs": {},
-    "networking": {},
-    "real_auto_mpg": {},
-    "real_cites": {},
-    "real_yacht": {},
-    "simulated_feedback": {"name": "Network1_amp", "version": 1},
-}
-# step 2:  set algorithm and it's parameters
-algo_param_dict = {
-    "pc": {"variant": None, "ci_test": None, "alpha": None, "priori_knowledge": None},
-    "ges": {"criterion": None, "method": None, "k": None, "N": None},
-    "icalingam": {},
-    "directlingam": {},
-    "anm": {},
-    "golem": {},
-    "grandag": {},
-    "notears": {},
-    "notearslowrank": {},
-    "notearsnonlinear": {},
-    "corl": {},
-    "rl": {},
-    "gae": {},
-    "pnl": {},
-}
-# step 3: set output file name
-OUTPUT_FILE_NAME = "yacht_castle"
-######################  Edit above area to set up experiment. #############################
+# use parameter file to set up experiment
+def build_full_path(string):
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    return os.path.normpath(os.path.join(script_dir, string))
 
 
-def init_func_with_param(func, kwargs):
+def init_func_with_param(func, kwargs: dict):
     if bool(kwargs):
         return func(**{k: v for k, v in kwargs.items() if v is not None})
-    return func()
+    else:
+        raise ValueError("kwargs can not be empty.")
 
 
-def load_datasest(dataset_name: str):
-    kwargs = dataset_list[dataset_name]
-    if dataset_name.lower().startswith("alarm"):
+def load_datasest(name: str, kwargs={}):
+    if name.lower() == "alarm":
         from causalbench.data.alarm.alarm_loader import load_alarm
 
         return init_func_with_param(load_alarm, kwargs)
-    elif dataset_name.lower().startswith("dream4"):
+    elif name.lower() == "dream4":
         from causalbench.data.dream4.dream4_loader import load_dream4
 
         return init_func_with_param(load_dream4, kwargs)
-    elif "jdk" == dataset_name.lower():
+    elif name.lower() == "jdk":
         from causalbench.data.jdk.jdk_loader import load_jdk
 
         return load_jdk()
-    elif "postgres" == dataset_name.lower():
+    elif name.lower() == "postgres":
         from causalbench.data.postgres.postgres_loader import load_postgres
 
         return load_postgres()
-    elif "sachs" == dataset_name.lower():
+    elif name.lower() == "sachs":
         from causalbench.data.sachs.sachs_loader import load_sachs
 
         return load_sachs()
-    elif "networking" == dataset_name.lower():
+    elif name.lower() == "networking":
         from causalbench.data.networking.networking_loader import load_networking
 
         return load_networking()
-    elif "real_yacht" == dataset_name.lower():
+    elif name.lower() == "real_yacht":
         from causalbench.data.real_yacht.real_yacht_loader import load_real_yacht
 
         return load_real_yacht()
-    elif "real_cities" == dataset_name.lower():
+    elif name.lower() == "real_cities":
         from causalbench.data.real_cites.real_cites_loader import load_real_cites
 
         return load_real_cites()
-    elif "real_auto_mpg" == dataset_name.lower():
+    elif name.lower() == "real_auto_mpg":
         from causalbench.data.real_auto_mpg.real_auto_mpg_loader import (
             load_real_auto_mpg,
         )
 
         return load_real_auto_mpg()
-    elif "simulated_feedback" == dataset_name.lower():
+    elif name.lower() == "simulated_feedback":
         from causalbench.data.simulated_feedback.simulated_feedback_loader import (
             load_feedback,
         )
 
         return load_feedback()
     else:
-        raise ValueError(f"Data set: {dataset_name} not found.")
+        raise ValueError(
+            f"Data set: {name} not found. Please check info.txt for supported datasets."
+        )
 
 
-def init_algo_from_gcastle(algo_name: str, var_num: int):
+def init_algo_from_gcastle(
+    name: str,
+    var_num: int,
+    kwargs: dict,
+):
     """_summary_
 
     Args:
-        algo_name (str): _description_
+        name (str): _description_
+        var_num (int): _description_
+        kwargs (dict, optional): _description_. Defaults to {}.
 
     Raises:
-        ValueError: _description_ß
+        ValueError: _description_
 
     Returns:
         _type_: _description_
     """
-    kwargs = algo_param_dict[algo_name]
-    if algo_name.lower().startswith("pc"):
+
+    if name.lower() == "pc":
         from castle.algorithms import PC
 
         return init_func_with_param(PC, kwargs)
-    elif algo_name.lower().startswith("ges"):
+    elif name.lower() == "ges":
         from castle.algorithms import GES
 
         return init_func_with_param(GES, kwargs)
-    elif algo_name.lower().startswith("icalingam"):
+    elif name.lower() == "icalingam":
         from castle.algorithms import ICALiNGAM
 
         return init_func_with_param(ICALiNGAM, kwargs)
-    elif algo_name.lower().startswith("directlingam"):
+    elif name.lower() == "directlingam":
         from castle.algorithms import DirectLiNGAM
 
         return init_func_with_param(DirectLiNGAM, kwargs)
-    elif algo_name.lower().startswith("anm"):  # pairwise
+    elif name.lower() == "anm":  # pairwise
         from castle.algorithms import ANMNonlinear
 
         return init_func_with_param(ANMNonlinear, kwargs)
     # These algorithms need GPU
-    elif algo_name.lower().startswith("golem"):
+    elif name.lower() == "golem":
         from castle.algorithms import GOLEM
 
         return init_func_with_param(GOLEM, kwargs)
-    elif algo_name.lower().startswith("grandag"):
+    elif name.lower() == "grandag":
         from castle.algorithms import GraNDAG
 
         kwargs["input_dim"] = var_num
         return init_func_with_param(GraNDAG, kwargs)
-    elif algo_name.lower().startswith("notears"):
+    elif name.lower() == "notears":
         from castle.algorithms import Notears
 
         return init_func_with_param(Notears, kwargs)
-    elif algo_name.lower().startswith("notearslowrank"):
+    elif name.lower() == "notearslowrank":
         from castle.algorithms import NotearsLowRank
 
         return init_func_with_param(NotearsLowRank, kwargs)
-    elif algo_name.lower().startswith("notearsnonlinear"):  # unclear
+    elif name.lower() == "notearsnonlinear":
         from castle.algorithms import NotearsNonlinear
 
         return init_func_with_param(NotearsNonlinear, kwargs)
-    elif algo_name.lower().startswith("corl"):
+    elif name.lower() == "corl":
         from castle.algorithms import CORL
 
         return init_func_with_param(CORL, kwargs)
-    elif algo_name.lower().startswith("rl"):
+    elif name.lower() == "rl":
         from castle.algorithms import RL
 
         return init_func_with_param(RL, kwargs)
-    elif algo_name.lower().startswith("gae"):
+    elif name.lower() == ("gae"):
         from castle.algorithms import GAE
 
-        return GAE()
-    elif algo_name.lower().startswith("pnl"):  # pairwise?
+        return init_func_with_param(GAE, kwargs)
+    elif name.lower() == ("pnl"):
         from castle.algorithms import PNL
 
         return init_func_with_param(PNL, kwargs)
@@ -193,15 +173,22 @@ def gen_output_file(path_result, file_name, dict_result):
             writer.writerow(dict_result)
 
 
-def run(dataset_name: str, algo_name: str, path_result):
+def run(
+    algo_name: str,
+    algo_kwargs: dict,
+    dataset_name: str,
+    dataset_kwargs: dict,
+    path_result: str,
+    output_file_name: str,
+):
     # load data set
-    dataset = load_datasest(dataset_name)
+    dataset = load_datasest(dataset_name, dataset_kwargs)
     true_causal_matrix = dataset["true_matrix"]
     X = dataset["X"]
-    logging.info("%s loaded", dataset_name)
+    logging.info("%s loaded", dataset["name"])
     # init algorithm
     var_num = dataset["var_num"]
-    algo = init_algo_from_gcastle(algo_name, var_num)
+    algo = init_algo_from_gcastle(algo_name, var_num, algo_kwargs)
     logging.info("%s algorithm initiated.", algo_name)
     # structure learning
     starttime = time.perf_counter()
@@ -218,7 +205,8 @@ def run(dataset_name: str, algo_name: str, path_result):
         "varsortability": dataset["varsortability"],
         "N_variables": X.shape[1],
         "N_obs": X.shape[0],
-        "model_name": algo_name.lower(),
+        "algo_name": algo_name.lower(),
+        "algo_param": algo_kwargs,
         "library_name": "gCastle",
         "fdr": mt.metrics["fdr"],
         "tpr": mt.metrics["tpr"],
@@ -232,14 +220,15 @@ def run(dataset_name: str, algo_name: str, path_result):
         "runtime_second": runtime,
         "experiment_time": time.ctime(),
     }
-    gen_output_file(path_result, f"{OUTPUT_FILE_NAME}.csv", dict_result)
+    gen_output_file(path_result, f"{output_file_name}.csv", dict_result)
 
 
 def main():
-    algo_name_list = list(algo_param_dict.keys())
-    logging.info(f"algorithms are: {algo_name_list}")
-    dataset_name_list = list(dataset_list.keys())
-    logging.info(f"data set are: {dataset_name_list}")
+    algorithms = params["algorithms"][0]["gcastle"]
+    logging.info("algorithms are: %s", algorithms)
+    datasets = params["datasets"]
+    logging.info("data set are: %s", datasets)
+    output_file_name = params["OUTPUT_FILE_NAME"]
     # set output file path
     file_dir = os.path.dirname(__file__)
     path_result = os.path.join(file_dir, "result")
@@ -250,13 +239,28 @@ def main():
     else:
         logging.info("result dir created.")
 
-    task_list = combine_multiple_lists([dataset_name_list, algo_name_list])
+    tasks = combine_multiple_lists([algorithms, datasets])
     with concurrent.futures.ProcessPoolExecutor() as executor:
-        for task in task_list:
-            executor.submit(run, task[0], task[1], path_result)
-    # for task in task_list:
-    #     run(task[0], task[1], path_result)
+        for task in tasks:
+            executor.submit(
+                run,
+                task[0]["name"],
+                task[0]["kwargs"],
+                task[1]["name"],
+                task[1]["kwargs"],
+                path_result,
+                output_file_name,
+            )
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="CausalBench")
+    parser.add_argument("-src", required=True, help="relative path to parameter file")
+    param_file_path = build_full_path(parser.parse_args().src)
+    logging.info("file to read: %s", param_file_path)
+
+    params = None
+    with open(param_file_path) as json_file:
+        params = json.load(json_file)
+
     main()
